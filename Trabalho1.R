327de68225c49aafe034e38af7b1d4a593ce51b0
thiagom <thiagom@motorola.com>
2018-08-07 10:33:08 -0300

    Added a few changes

--- Trabalho1.R
+++ Trabalho1.R
@@ -13,9 +13,24 @@ val_data<- read.csv("housePricing_valSet.csv", header = TRUE)
 
 summary(train_data)
 summary(val_data)
+
 # Remove entries with N/A 
 train_data <- train_data[!is.na(train_data$total_bedrooms),]
 val_data <- val_data[!is.na(val_data$total_bedrooms),]
+
+
+# Normalize the data (Removing ocean_proximity and median_house_value)
+meanTrainFeatures = colMeans(train_data[,-(9:10)]) #mean of each feature
+stdTrainFeatures = apply(train_data[,-(9:10)], 2, sd) #std of each feature
+
+meanTrainFeatures
+stdTrainFeatures
+
+train_data[,-(9:10)] = sweep(train_data[,-(9:10)], 2, meanTrainFeatures, "-")
+train_data[,-(9:10)] = train_data[,-(9:10)] / stdTrainFeatures
+
+val_data[,-(9:10)] = sweep(val_data[,-(9:10)], 2, meanTrainFeatures, "-")
+val_data[,-(9:10)] = val_data[,-(9:10)] / stdTrainFeatures
 
 # Changing Discrete Data
 train_data$h1ocean <- as.numeric(train_data$ocean_proximity == "<1H OCEAN")
@@ -30,29 +45,18 @@ val_data$island <- as.numeric(val_data$ocean_proximity == "ISLAND")
 val_data$near_bay <- as.numeric(val_data$ocean_proximity == "NEAR BAY")
 val_data$near_ocean <- as.numeric(val_data$ocean_proximity == "NEAR OCEAN")
 
+train_data$ocean_proximity = NULL
+val_data$ocean_proximity = NULL
 
-# Normalize the data (Removing )
-meanTrainFeatures = colMeans(train_data[,-(9:10)]) #mean of each feature
-stdTrainFeatures = apply(train_data[,-(9:10)], 2, sd) #std of each feature
-
-meanTrainFeatures
-stdTrainFeatures
-
-train_data[,-(9:10)] = sweep(train_data[,-(9:10)], 2, meanTrainFeatures, "-")
-train_data[,-(9:10)] = train_data[,-(9:10)] / stdTrainFeatures
-
-val_data[,-(9:10)] = sweep(val_data[,-(9:10)], 2, meanTrainFeatures, "-")
-val_data[,-(9:10)] = val_data[,-(9:10)] / stdTrainFeatures
-
-train_data <- train_data[,-10]
-val_data <- val_data[,-10]
+cor(train_data[,-9])
 
 # Calculate Simple Linear Regression (Trainning)
-prediction_simple <- lm(formula = median_house_value ~ longitude + latitude + housing_median_age + 
-                          total_bedrooms + total_rooms + population + households + median_income +
-                          h1ocean + inland + island + near_bay + near_ocean, data = train_data)
+prediction_simple <- lm(formula = median_house_value ~ ., data = train_data)
 
-result_prediction <- predict(prediction_simple, val_data[,-9])
+result_prediction <- predict(prediction_simple, val_data)
 summary(result_prediction)
 
 mae_simple <- sum(abs(result_prediction - val_data$median_house_value)) / length(result_prediction)
+mae_simple
+
+summary(prediction_simple)
