bd31d2be1c60bdac2e95534845d5b6cd583bbabb
Ribeiro Rafael-RAFAELR <rafaelr@motorola.com>
2018-08-10 09:15:23 -0300

    add test data

--- Trabalho1.R
+++ Trabalho1.R
@@ -10,6 +10,7 @@ setwd("C:\\Users\\rafaelr\\Documents\\INF015\\Tarefa1\\INF0615_Tarefa1")
 # Reading data
 train_data <- read.csv("housePricing_trainSet.csv", header = TRUE)
 val_data<- read.csv("housePricing_valSet.csv", header = TRUE)
+test_data<- read.csv("housePricing_testSet.csv", header = TRUE)
 
 dim(train_data)
 summary(train_data)
@@ -19,7 +20,7 @@ summary(val_data)
 # Remove entries with N/A 
 train_data <- train_data[!is.na(train_data$total_bedrooms),]
 val_data <- val_data[!is.na(val_data$total_bedrooms),]
-
+test_data <- test_data[!is.na(test_data$total_bedrooms),]
 
 # Normalize the data (Removing ocean_proximity and median_house_value)
 meanTrainFeatures = colMeans(train_data[,-(9:10)]) #mean of each feature
@@ -34,6 +35,9 @@ train_data[,-(9:10)] = sweep(train_data[,-(9:10)], 2, meanTrainFeatures, "/")
 val_data[,-(9:10)] = sweep(val_data[,-(9:10)], 2, meanTrainFeatures, "-")
 val_data[,-(9:10)] = sweep(val_data[,-(9:10)], 2, meanTrainFeatures, "/")
 
+test_data[,-(9:10)] = sweep(test_data[,-(9:10)], 2, meanTrainFeatures, "-")
+test_data[,-(9:10)] = sweep(test_data[,-(9:10)], 2, meanTrainFeatures, "/")
+
 # Changing Discrete Data
 train_data$h1ocean <- as.numeric(train_data$ocean_proximity == "<1H OCEAN")
 train_data$inland <- as.numeric(train_data$ocean_proximity == "INLAND")
@@ -47,8 +51,15 @@ val_data$island <- as.numeric(val_data$ocean_proximity == "ISLAND")
 val_data$near_bay <- as.numeric(val_data$ocean_proximity == "NEAR BAY")
 val_data$near_ocean <- as.numeric(val_data$ocean_proximity == "NEAR OCEAN")
 
+test_data$h1ocean <- as.numeric(test_data$ocean_proximity == "<1H OCEAN")
+test_data$inland <- as.numeric(test_data$ocean_proximity == "INLAND")
+test_data$island <- as.numeric(test_data$ocean_proximity == "ISLAND")
+test_data$near_bay <- as.numeric(test_data$ocean_proximity == "NEAR BAY")
+test_data$near_ocean <- as.numeric(test_data$ocean_proximity == "NEAR OCEAN")
+
 train_data$ocean_proximity = NULL
 val_data$ocean_proximity = NULL
+test_data$ocean_proximity = NULL
 
 cor(train_data[,-9])
 
@@ -60,6 +71,12 @@ summary(result_prediction)
 
 mae_simple <- sum(abs(result_prediction - val_data$median_house_value)) / length(result_prediction)
 mae_simple
+
+test_prediction <- predict(prediction_simple, test_data)
+summary(test_prediction)
+
+mae_simple_test <- sum(abs(test_prediction - test_data$median_house_value)) / length(test_prediction)
+mae_simple_test
 
 summary(prediction_simple)
 
@@ -78,6 +95,13 @@ summary(complex_res)
 
 mae_complex <- sum(abs(complex_res - val_data$median_house_value)) / length(complex_res)
 mae_complex
+
+
+test_prediction <- predict(prediction_complex, test_data)
+summary(test_prediction)
+
+mae_complex_test <- sum(abs(test_prediction - test_data$median_house_value)) / length(test_prediction)
+mae_complex_test
 
 summary(prediction_complex)
 
@@ -98,5 +122,11 @@ summary(complex_res2)
 
 mae_complex2 <- sum(abs(complex_res2 - val_data$median_house_value)) / length(complex_res2)
 mae_complex2
+
+test_prediction2 <- predict(prediction_complex2, test_data)
+summary(test_prediction2)
+
+mae_complex_test2 <- sum(abs(test_prediction2 - test_data$median_house_value)) / length(test_prediction2)
+mae_complex_test2
 
 summary(prediction_complex2)
